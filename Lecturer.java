ada6235aeb6012670032913b000f7a1604a142fc
mor bargig <morbargig@gmail.com>
2020-05-20 14:46:25 +0300

    autocommit

--- Lecturer.java
+++ Lecturer.java
@@ -1,27 +1,27 @@
-public class Lecturer {
-    private String name;
-    private Double payPerHour;
+// public class Lecturer {
+//     private String name;
+//     private Double payPerHour;
 
-    public Lecturer(String name, Double payPerHour) {
-        this.setName(name);
-        this.setPayPerHour(payPerHour);
-    }
+//     public Lecturer(String name, Double payPerHour) {
+//         this.setName(name);
+//         this.setPayPerHour(payPerHour);
+//     }
 
-    public Double getPayPerHour() {
-		return payPerHour;
-	}
+//     public Double getPayPerHour() {
+// 		return payPerHour;
+// 	}
 
-	public void setPayPerHour(Double payPerHour) {
-		this.payPerHour = payPerHour;
-	}
+// 	public void setPayPerHour(Double payPerHour) {
+// 		this.payPerHour = payPerHour;
+// 	}
 
-    public void setName(String name){
-        this.name = name;
-    }
+//     public void setName(String name){
+//         this.name = name;
+//     }
 
-	public String getName() {
-        return name;
-    }
+// 	public String getName() {
+//         return name;
+//     }
 
 
-}
+// }
